acb0e1149ad30b5c4f8a702e06b4edae8d7fa60d
igorcastro1304 <igor.hoffmann@al.infnet.edu.br>
2024-08-05 21:56:09 -0400

    feature/header-component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,178 @@
+import React from "react";
+import {
+    Navbar,
+    MobileNav,
+    Typography,
+    Button,
+    IconButton,
+    Card,
+} from "@material-tailwind/react";
+import { logout } from "../../utils/auth";
+import { Link } from "react-router-dom";
+
+export function Header(props) {
+
+    const { userIsLoggedIn } = props;
+
+    console.log(userIsLoggedIn);
+
+    const [openNav, setOpenNav] = React.useState(false);
+
+    React.useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setOpenNav(false),
+        );
+    }, []);
+
+    const navList = (
+        <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            <Typography
+                as="li"
+                variant="small"
+                className="p-1 font-normal text-yellow-400"
+            >
+                <a href="#" className="flex items-center">
+
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                className="p-1 font-normal text-yellow-400"
+            >
+                <a href="#" className="flex items-center">
+                    {userIsLoggedIn ? "Dashboard" : "Sobre Nós"}
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                className="p-1 font-normal text-yellow-400"
+            >
+                <a href="#" className="flex items-center">
+                    {userIsLoggedIn ? "Usuários" : "Contate-nos"}
+                </a>
+            </Typography>
+            <Typography
+                as="li"
+                variant="small"
+                className="p-1 font-normal text-yellow-400"
+            >
+                <a href="#" className="flex items-center">
+                    {userIsLoggedIn ? "Estoque" : "Política de Privacidade"}
+                </a>
+            </Typography>
+        </ul>
+    );
+
+    return (
+        <div className="max-h-[768px] w-screen bg-black">
+            <Navbar className=" z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+                <div className="flex items-center justify-between text-blue-gray-900">
+                    <Link to="/"
+                        className="mr-4 cursor-pointer py-1.5 font-medium text-yellow-400"
+                    >
+                        Logo
+                    </Link>
+                    <div className="flex items-center gap-4">
+                        <div className="mr-4 hidden lg:block">{navList}</div>
+                        <div className="flex items-center gap-x-1">
+                            {userIsLoggedIn ?
+                                (
+                                    <div>
+                                        <Button
+                                            variant="text"
+                                            size="sm"
+                                            className="hidden lg:inline-block text-yellow-400"
+                                            onClick={() => logout()}
+                                        >
+                                            <span>Logout</span>
+                                        </Button>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <Button
+                                            variant="text"
+                                            size="sm"
+                                            className="hidden lg:inline-block text-yellow-400"
+                                        >
+                                            <Link to="login">Cadastro</Link>
+                                        </Button>
+                                        <Button
+                                            variant="gradient"
+                                            size="sm"
+                                            className="hidden lg:inline-block text-yellow-400"
+                                        >
+                                            <Link to="login">Login</Link>
+                                        </Button>
+                                    </div>
+                                )}
+
+
+                        </div>
+                        <IconButton
+                            variant="text"
+                            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                            ripple={false}
+                            onClick={() => setOpenNav(!openNav)}
+                        >
+                            {openNav ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    className="h-6 w-6 text-yellow-400"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 text-yellow-400"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </IconButton>
+                    </div>
+                </div>
+                <MobileNav open={openNav}>
+                    {navList}
+                    <div className="flex items-center flex-row gap-x-1 justify-end">
+                        {userIsLoggedIn ? (
+                            <div>
+                                <Button fullWidth variant="text" size="sm" className="text-yellow-400" onClick={() => logout()}>
+                                    <span>Logout</span>
+                                </Button>
+                            </div>
+                        ) : (
+                            <div className="flex items-center flex-row gap-x-1 justify-end">
+                                <Button fullWidth variant="text" size="sm" className="text-yellow-400">
+                                    <Link to="login">Cadastro</Link>
+                                </Button>
+                                <Button fullWidth variant="gradient" size="sm" className="text-yellow-400">
+                                    <Link to="login">Login</Link>
+                                </Button>
+                            </div>
+                        )}
+
+                    </div>
+                </MobileNav>
+            </Navbar>
+        </div>
+    );
+}
