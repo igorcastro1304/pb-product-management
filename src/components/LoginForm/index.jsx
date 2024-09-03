748d4b4b7efd21ed7e34f991735d0457f0c4feac
igorcastro1304 <131926596+igorcastro1304@users.noreply.github.com>
2024-09-02 23:12:48 -0400

    feature/created-auth-context: Foi criado o contexto de autenticação, tornando mais fácil e modularizada o gerenciamento da autenticação (#4)

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,31 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../Button";
-import { login } from "../../utils/auth";
+import { useAuth } from "../../contexts/AuthContext";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { redirect, useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const { login, currentUser } = useAuth();
+  const navigate = useNavigate();
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        try {
-            login(email, password);
-        } catch (err) {
-            console.log(err);
-        } finally {
-            setEmail("");
-            setPassword("");
-        }
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/");
     }
+  }, [currentUser]);
 
-    return (
-        <form className="w-[20rem] p-4 flex flex-col justify-center gap-2 md:w-[30rem] lg:w-[40rem]" onSubmit={handleSubmit}>
-            <input className="p-1 rounded-sm border-black/70" placeholder="Digite seu e-mail" value={email} onChange={(event) => setEmail(event.target.value)} />
-            <input className="p-1 rounded-sm border-black/70" type="password" placeholder="Digite sua senha" value={password} onChange={(event) => setPassword(event.target.value)} />
+  function handleSubmit(e) {
+    e.preventDefault();
 
-            <Button type="submit" className="bg-black text-yellow-400 p-2 rounded-sm">Fazer login</Button>
+    try {
+      setIsLoading(true);
+      login(email, password);
+      toast("Usuário logado com sucesso!");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+      setEmail("");
+      setPassword("");
+    }
+  }
 
-        </form>
-    );
+  return (
+    <form
+      className="w-[20rem] p-4 flex flex-col justify-center gap-2 md:w-[30rem] lg:w-[40rem]"
+      onSubmit={handleSubmit}
+    >
+      <ToastContainer />
+      <input
+        className="p-1 rounded-sm border-black/70"
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        className="p-1 rounded-sm border-black/70"
+        type="password"
+        placeholder="Digite sua senha"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <Button type="submit" className="p-2 text-yellow-400 bg-black rounded-sm">
+        Fazer login
+      </Button>
+    </form>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import LoginPage from './pages/login/index.jsx';
+import LoginPage from "./pages/login/index.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,8 +19,10 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
